7397ab0c578b10bc309f27bb6fc30c234c01abb7
Tsvetelina Yordanova <49713089+tsvetelina-e-y@users.noreply.github.com>
2020-09-29 12:08:12 +0300

    fix: variables compilation err

--- Variables.java
+++ Variables.java
@@ -17,46 +17,46 @@ public class Variables {
     public static void main(String[] args) {
 
         byte personAge = 21;
-        System.out.println("Age: " + personAge + "\n");
+        System.out.println("Age: " + personAge);
 
         byte grade = 6, temperature = -5;
         System.out.printf("My grade is %d.The temperature is %d.\n\n", grade, temperature);
         
         short distanceBetweenSofiaAndBerlin;     
         distanceBetweenSofiaAndBerlin = 1634;      
-        System.out.println("The distance is: " + distanceBetweenSofiaAndBerlin + "km" + "\n");
+        System.out.println("The distance is: " + distanceBetweenSofiaAndBerlin + "km");
         
         int studentCount= 50000;       
-        System.out.printf("%d students have graduated\n", studentCount + "\n"); 
+        System.out.printf("%d students have graduated\n", studentCount); 
         
         long numberOfCells;
         numberOfCells = 37_200_000_000_000l;   
-        System.out.println("Number of cells: " + numberOfCells + "\n");
+        System.out.println("Number of cells: " + numberOfCells);
         
         float water = 1.85f;    
-        System.out.println("We have " + water+ " liters of water" + "\n");
+        System.out.println("We have " + water+ " liters of water");
         
         double sunTemperature = 15000000.1212;
-        System.out.println("Sun temperature is: " + sunTemperature + "\n");
+        System.out.println("Sun temperature is: " + sunTemperature);
         
         double cash = 3.50;
-        System.out.printf("I have %.2f leva.\n", cash + "\n");
+        System.out.printf("I have %.2f leva.\n", cash);
         
         boolean agreed = true;
         agreed = false;
-        System.out.println("Agreed: "+ agreed + "\n");
+        System.out.println("Agreed: "+ agreed);
         
         boolean buyBeer = personAge >= 18;
-        System.out.println("Can buy beer: " + buyBeer + "\n");
+        System.out.println("Can buy beer: " + buyBeer);
         
         char gender = 'M';
         
         String myName = "Tsvetelina Yordanova";
-        System.out.println("My name is: " + myName + "\n");
+        System.out.println("My name is: " + myName);
         
         String job;
         job = "Sofware developer";
-        System.out.printf("I work as %s\n\n", job);  
+        System.out.printf("I work as %s \n", job);  
 
     }
 
